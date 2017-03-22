f1bc54684bc98257b09f2ec540232ac831d51bd0
Donnie West <me@donniewest.com>
2017-03-21 23:43:27 -0500

    Add rrc and have posts scroll into the view of the user

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import dateFormatter from '../../utils/dateFormatter'
+import { ScrollIntoView } from 'rrc';
 import './Post.css'
 
 export default ({ post }) => (
   <div>
+    <ScrollIntoView id="#title" />
     <div className='article-entry'>
-      <h1 className='article-title'>{post.title}</h1>
+      <h1 className='article-title' id="title">{post.title}</h1>
       <p className='date'>{dateFormatter(new Date(post.date))}</p>
       <p className='article-read-time'>
         {`${Math.ceil(post.content.split(' ').length / 200)} minutes to read`}
