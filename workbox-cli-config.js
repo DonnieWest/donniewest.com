544ea8b168224392292d69f274049b6a53c44a29
Donnie West <me@donniewest.com>
2017-06-04 19:24:48 -0500

    Switch to workbox and bump dependencies

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "build/",
+  "globPatterns": [
+    "**/*.{html,png,json,xml,ico,svg,txt,js,css,map}"
+  ],
+  "swDest": "build/sw.js",
+  "globIgnores": [
+    "../workbox-cli-config.js"
+  ]
+};
