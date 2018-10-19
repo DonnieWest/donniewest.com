788bf5600907a3e27b528c667362de06b8a02ee9
Donnie West <me@donniewest.com>
2018-10-18 20:56:48 -0500

    Fix up Inferno further

--- gatsby-node.js
+++ gatsby-node.js
@@ -66,12 +66,12 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
   // Requiring the server version of React-dom is hardcoded right now
   // in the development server. So we'll just avoid loading Inferno there
   // for now.
-  if (stage !== `develop-html`) {
+  if (stage !== `develop-html` && stage !== 'develop') {
     actions.setWebpackConfig({
       resolve: {
         alias: {
-          react: 'inferno',
-          'react-dom': 'inferno',
+          react: 'inferno-compat',
+          'react-dom': 'inferno-compat',
         },
       },
     })
